287174fe4c5f60520d94f02bf0fa372605157000
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-11 21:56:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,17 +10,20 @@ end
 
 def using_reverse(array)
   using_reverse = ["wow", "I", "am", "really", "learning", "arrays!"]
-  using_reverse.reverse
+  using_reverse.reverse!
 end
 
 def using_first(array)
-
+  using_first = ["wow", "I", "am", "really", "learning", "arrays!"]
+  using_first.first!
 end
 
 def using_last(array)
-
+  using_last = ["wow", "I", "am", "really", "learning", "arrays!"]
+  using_last.last!
 end
 
 def using_size(array)
-
+  using_size = ["wow", "I", "am", "really", "learning", "arrays!"]
+  using_size.size?
 end
